225636406b1815f5ca39790ba8629f0db3b0d1f4
Ratan Das <63622873+TheRatanDas@users.noreply.github.com>
2021-09-21 14:06:23 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,24 +1,9 @@
-const got = require('got');
 const slugify = require('slugify');
 const ytdl = require('ytdl-core');
 
-const video = "https://www.facebook.com/LyricsEngsongs/videos/321854395918041/";
 const YT_VIDEO = "https://www.youtube.com/watch?v=bKDdT_nyP54";
 
 (async () => {
-    try {
-        const response = await got(video);
-        const seo_title = response.body.split('<title id="pageTitle">')[1].split('</title>')[0];
-        const title = slugify(seo_title, {
-            replacement: '-',
-            remove: /[*+~.()'"!:@]/g,
-            lower: true,
-            strict: true
-        });
-        console.log(title);
-    } catch (error) {
-        console.log("null");
-    }
     try {
         var url = YT_VIDEO;
         if (!ytdl.validateURL(url)) {
